d2208929e6580908bbbb275e86f1623a5f08b0ec
ashrafulrcr43 <124384576+ashrafulrcr43@users.noreply.github.com>
2024-05-17 23:39:20 +0600

    update done

--- oop.php
+++ oop.php
@@ -1,67 +1,92 @@
-<?php 
+<?php  
 class Book {
- // TODO: Add properties as Private
- private $title=["The Great Gatsby","To Kill a Mockingbird"];
- private $availableCopies;
- 
- public function __construct($title, $availableCopies) {
-     // TODO: Initialize properties
-     $this->title=$title;
-     $this->availableCopies=$availableCopies = 0;
-     }
-// TODO: Add getTitle method
-public function getTitle(){
+    // TODO: Add properties as Private
+    private $title;
+    private $availableCopies;
+
     
+    public function __construct($title, $availableCopies=0) {
+        // TODO: Initialize properties
+        $this->title = $title;
+        $this->availableCopies = $availableCopies;
+
+        }
+   
+   
+   // TODO: Add getTitle method
+   public function getTitle(){
+       return $this->title;
+
+   }
+   
+   // TODO: Add getAvailableCopies method
+   public function getAvailableCopies(){
+     
+       return $this->availableCopies;
+   }
+   
+    // TODO: Add borrowBook method
+    public function borrowBook(){
+        $this->availableCopies--;
+        if($this->availableCopies > 0){
+            $this->availableCopies--;
+    }
+   
 }
- 
+   
+    // TODO: Add returnBook method
+    public function returnBook(){
+        $this->availableCopies++;
+    }
+    
+   }
+   
+   $book1 =new Book("Name: The Great Gatsby, Available Copies:",5);
+   $book1->borrowBook(1);
+   $book1->returnBook();
+   echo "Available Copies of 'The Great Gatsby':".$book1->getAvailableCopies();
+    echo PHP_EOL;
 
- // TODO: Add getAvailableCopies method
- public function getAvailableCopies($availableCopies){
-    $this->availableCopies=$availableCopies;
- }
+   $book2 =new Book("Name: To Kill a Mockingbird, Available Copies:",3);
+   $book2->borrowBook(2);
+   $book2->returnBook();
+   echo "Available Copies of 'To Kill a Mockingbird':".$book2->getAvailableCopies();
+    echo PHP_EOL;
+   
+   class Member {
+    // TODO: Add properties as Private
+    private $name;
 
- // TODO: Add borrowBook method
- public function borrowBook($availableCopies){
-    $this->availableCopies-=$availableCopies;
- }
- 
+   
+    public function __construct($name) {
+         // TODO: Initialize properties
+         $this->name = $name;
+        }
+   
+   
+   
+    // TODO: Add getName method
+    public function getName(){
+        return $this->name;
+    }
+    
+    // TODO: Add borrowBook method
+    public function borrowBook(){
+        $this->name = "Member Name: " . $this->name;
+    }
+    
+   
+   
+   
+    // TODO: Add returnBook method
+    public function returnBook(){
+        $this->name = "Member Name: " . $this->name;
+    }
+    
+   }
+   
+   $Member1 =new Member("Name: John Doe");
+   $Member2 =new Member("Name: Jane Smith");
+   $Member1->borrowBook();
+   $Member2->borrowBook();
 
- // TODO: Add returnBook method
- public function returnBook($availableCopies){
-    $this->availableCopies+=$availableCopies;
- }
- 
-}
-
-class Member {
- // TODO: Add properties as Private
-
- public function __construct($name) {
-      // TODO: Initialize properties
-     }
-
- // TODO: Add getName method
- 
- // TODO: Add borrowBook method
- 
-
- // TODO: Add returnBook method
- 
-}
-
-
-/*
-// Usage
-// You have to create  2 books and 2 members. Members One should borrow  book 1 and Member Two should borrow  book 2.
-// TODO: Create 2 books with the following properties
-Book 1 - Name: The Great Gatsby, Available Copies: 5.
-Book 2 - Name: To Kill a Mockingbird, Available Copies: 3.
-
-// TODO: Create 2 members with the following properties
-Member 1 - Name: John Doe
-Member 2 - Name: Jane Smith
-
-// TODO: Apply Borrow book method to each member
-
-// TODO: Print Available Copies with their names:
-*/
